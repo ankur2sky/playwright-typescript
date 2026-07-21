63ebf330b99d1a332cfe0ed66f4e65b97ce7897a
ankur2sky <ankurrathore4u@gmail.com>
2026-07-21 18:49:44 +0530

    waitUntil added

--- tests/browser/backandforward.spec.ts
+++ tests/browser/backandforward.spec.ts
@@ -17,7 +17,7 @@ import { test, expect } from '@playwright/test';
 
 test('Back and forward browser navigation', async ({ page }) => {
 
-    await page.goto("https://naveenautomationlabs.com/opencart/index.php?route=account/login");
+    await page.goto("https://naveenautomationlabs.com/opencart/index.php?route=account/login",{waitUntil:"load"});
     console.log(await page.title());
     console.log(await page.url());
     await page.waitForTimeout(3000);
