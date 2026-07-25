e51b539827a0fa6c54a73eefc717a32f42499943
ankur2sky <ankurrathore4u@gmail.com>
2026-07-25 20:18:35 +0530

    locator test case commit

---
+++ tests/browser/locator.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('locator', async ({ page }) => {
+
+    let Desktoplinks=page.getByRole('link',{name:'Desktops'})
+    let LaptopandNotebooklinks=page.getByRole('link',{name:'Laptops & Notebooks'})
+    let componentslink = page.getByRole('link',{name:'Components'})
+    let tabletlinks = page.getByRole('link',{name:'Tablets'})
+    
+
+
+
+});
